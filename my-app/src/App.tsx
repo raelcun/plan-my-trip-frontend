26b2f6bcbc09ae087d35b1e6864acc641c10f831
Nhan Mai <maihuunhan1990@gmail.com>
2021-09-11 17:11:39 -0400

    add react typescript

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import HelloWorld from './componets/helloWorld';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
