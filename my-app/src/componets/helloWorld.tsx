1e13644c5312c79782afd6255e4e2221063a52dc
Nhan Mai <maihuunhan1990@gmail.com>
2021-10-25 16:45:50 -0400

    take 6

--- my-app/src/componets/helloWorld.tsx
+++ my-app/src/componets/helloWorld.tsx
@@ -1,2 +1,2 @@
-const HelloWorld = () => <h1>Plan My Trip, Take 5. Testing source control</h1>;
+const HelloWorld = () => <h1>Plan My Trip, Take 6. Testing source control</h1>;
 export default HelloWorld;
