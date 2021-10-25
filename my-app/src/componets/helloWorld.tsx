375fdaf0686d4bfecc48beaf04876bbb47642ecf
Nhan Mai <maihuunhan1990@gmail.com>
2021-10-25 16:33:38 -0400

    update hello world string

--- my-app/src/componets/helloWorld.tsx
+++ my-app/src/componets/helloWorld.tsx
@@ -1,2 +1,2 @@
-const HelloWorld = () => <h1>Plan My Trip, Take 4. Testing source control</h1>;
+const HelloWorld = () => <h1>Plan My Trip, Take 5. Testing source control</h1>;
 export default HelloWorld;
